62ee648ac23e5eb31408fcf37fa9aae542f2516a
junoko <gurumee328@gmail.com>
2018-07-24 13:19:53 +0900

    Revert "add:"
    This reverts commit b2b2533ed35e8eaf6c449fb533b1a1c8dc437ea3.

--- common/DNN_test.py
+++ common/DNN_test.py
@@ -6,32 +6,27 @@ from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import shuffle
-import matplotlib.pyplot as plt
 
-data = pd.read_csv("football_players_test1.csv")
+data = pd.read_csv("football_players_test.csv")
 
 data = shuffle(data)
 
 data.info()
 
 X = data.drop("Club_Position",axis=1)
-X = X.drop("Club_Position1",axis=1)
-y = data["Club_Position1"]
+y = data["Club_Position"]
 
-X.head()
-y.head()
-
-# pca = PCA(n_components=15)
+# pca = PCA(n_components=8)
 # X = pca.fit_transform(X)
 
-# mdict = dict(enumerate(y.unique()))
-# y.replace(to_replace=mdict.values(),value=mdict.keys(),inplace=True)
+mdict = dict(enumerate(y.unique()))
+y.replace(to_replace=mdict.values(),value=mdict.keys(),inplace=True)
 
 X=StandardScaler().fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=43)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# sm = SMOTE()
+# sm = SMOTE(random_state=42)
 # X_train, y_train = sm.fit_sample(X_train, y_train.ravel())
 
 print(np.shape(X_train))
@@ -53,27 +48,26 @@ Y_one_hot = tf.reshape(Y_one_hot,[-1,nbclass])
 
 keep_prob = tf.placeholder(tf.float32)
 
-size_l1 =1024
+size_l1 =256
 size_l2 = size_l1/2
 size_l3 =size_l2/2
 size_l4 =size_l3/2
 
-rate = 0.3
 l1 = tf.layers.dense(X,size_l1,activation=tf.nn.relu,bias_initializer=tf.contrib.layers.xavier_initializer())
-l1 = tf.layers.dropout(l1,rate=rate)
+# l1 = tf.layers.batch_normalization(l1)
 l2 = tf.layers.dense(l1,size_l2,activation=tf.nn.relu,bias_initializer=tf.contrib.layers.xavier_initializer())
-l2 = tf.layers.dropout(l2,rate=rate)
+# l2 = tf.layers.batch_normalization(l2)
 l3 = tf.layers.dense(l2,size_l3,activation=tf.nn.relu,bias_initializer=tf.contrib.layers.xavier_initializer())
-l3 = tf.layers.dropout(l3,rate=rate)
+# l3 = tf.layers.batch_normalization(l3)
 l4 = tf.layers.dense(l3,size_l4,activation=tf.nn.relu,bias_initializer=tf.contrib.layers.xavier_initializer())
-l4 = tf.layers.dropout(l4,rate=rate)
+# l4 = tf.layers.batch_normalization(l4)
 l5 = tf.layers.dense(l1,nbclass,activation=None)
 
 hypothesis = tf.nn.softmax(l5)
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=l5,labels=Y_one_hot))
 
-optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
 
 correct_pred = tf.equal(tf.argmax(hypothesis,1),tf.argmax(Y_one_hot,1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))
@@ -83,11 +77,14 @@ print("train start")
 
 sess = tf.Session()
 
+tf.summary.merge_all()
+train_writer = tf.summary.FileWriter("./tensorboard/train_wine",sess.graph)
+
 init = tf.global_variables_initializer()
 sess.run(init)
 
 epoch = 200000
-batch_size = 128
+batch_size = 64
 mok = int(len(X_train) / batch_size)
 
 index = 1
@@ -106,6 +103,8 @@ for i in range(epoch*mok):
 
     if i % mok == 0:
         loss, acc = sess.run([cost, accuracy], feed_dict={X: X_train, Y: y_train})
-        print("<",i, ' :', 'loss : ', loss, 'acc : ', acc * 100,">")
-        loss,acc,hypo = sess.run([cost,accuracy,hypothesis],feed_dict={X: X_test,Y:y_test})
-        print(i, ' :', 'loss : ', loss, 'acc : ', acc*100,hypo[0:1]*100)
+        print(i, ' :', 'loss : ', loss, 'acc : ', acc * 100,"+++")
+        loss,acc = sess.run([cost,accuracy],feed_dict={X: X_test,Y:y_test})
+        print(i, ' :', 'loss : ', loss, 'acc : ', acc*100)
+
+train_writer.close()
