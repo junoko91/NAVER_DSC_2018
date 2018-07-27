069fd3d20fee82977ef0fdd8c44c4fce0617c820
HongSungMoon <hsm63746244@gmail.com>
2018-07-28 05:44:55 +0900

    fix :  - top_k 완성

--- src/smhong/scikit-learn/svm_top_k.py
+++ src/smhong/scikit-learn/svm_top_k.py
@@ -5,17 +5,18 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.utils import shuffle
 from sklearn import svm, metrics
 
-
-def top_n_accuracy(x_data, y_data, n, classes):
+def top_n_accuracy(train_data_pre, y_data, n):
+    print(np.argsort(train_data_pre, axis=1)[0:5])
+    train_best_n = np.argsort(train_data_pre, axis=1)[:, -n:]
     count = 0
-    print(x_data[0:5])
-#    print(y_data[0:5])
-    for i in range(x_data.shape[0]):
+    print(train_best_n[0:5])
+    print(y_data[0:5])
+    for i in range(train_best_n.shape[0]):
         idx = y_data[i]
-        if x_data[i, idx] >= classes - n :
-            #print('i :', i, ', idx :', idx, ', x_data[i, idx] :', x_data[i, idx], ', pivot :,', classes - n)
+        if (train_best_n[i] == idx).any() :
+            #print('i :', i, ', idx :', idx, ', x_data[i, idx] :', train_best_n[i])
             count += 1
-    return count / x_data.shape[0] * 100
+    return count / train_data_pre.shape[0] * 100
 
 
 data = pd.read_csv("../../../data/smhong/data_ver3.csv")
@@ -43,8 +44,7 @@ clf = svm.SVC(probability=True)
 clf.fit(X_train, Y_train)
 train_data_pre = clf.predict_proba(X_train)
 
-train_best_n = np.argsort(train_data_pre, axis=1)
-train_acc_score = top_n_accuracy(train_best_n, Y_train.values, 1, nbclass)
+train_acc_score = top_n_accuracy(train_data_pre, Y_train.values, 1)
 
 print("Train Data Top 3 Accuracy :", train_acc_score)
 
@@ -52,6 +52,7 @@ train_data_pre = clf.predict(X_train)
 print(train_data_pre[0:5])
 train_acc_score = metrics.accuracy_score(Y_train.values, train_data_pre)
 print("Train Data Accuracy :", train_acc_score)
+
 '''
 train_acc_score = metrics.accuracy_score(Y_train, train_data_pre)
 print("Train Data Accuracy :", train_acc_score)
